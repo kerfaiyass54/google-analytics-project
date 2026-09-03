e5745b43cf52b237f378b6f35de9fd9d07bd3613
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2026-09-03 17:09:55 +0100

    Added project environment

---
+++ analytics-data-management/src/main/java/com/googleanalytics/analyticsforgoogle/AnalyticsforgoogleApplication.java
@@ -0,0 +1,13 @@
+package com.googleanalytics.analyticsforgoogle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyticsforgoogleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyticsforgoogleApplication.class, args);
+	}
+
+}
