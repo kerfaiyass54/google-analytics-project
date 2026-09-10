9ff990156513957d0277f312e920a6425cc4af17
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2026-09-10 15:48:32 +0100

    Added home page component

--- google-analytics-ui/src/app/app.routes.ts
+++ google-analytics-ui/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './shared/components/home-page/home-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomePage,
+    children: [
+    ],
+  },
+];
