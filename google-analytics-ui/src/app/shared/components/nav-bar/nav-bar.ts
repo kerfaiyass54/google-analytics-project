a37b2c2264176e32c9a483153f51b907300333d8
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2026-09-10 16:00:23 +0100

    Added home page component

--- google-analytics-ui/src/app/shared/components/nav-bar/nav-bar.ts
+++ google-analytics-ui/src/app/shared/components/nav-bar/nav-bar.ts
@@ -1,9 +1,34 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.css',
 })
-export class NavBar {}
+export class NavBar {
+  readonly navigationItems = [
+    {
+      label: 'Home',
+      icon: 'bi-grid-1x2-fill',
+      route: '/dashboard',
+    },
+    {
+      label: 'Apps',
+      icon: 'bi-phone-fill',
+      route: '/applications',
+    },
+    {
+      label: 'Operations',
+      icon: 'bi-sliders2-vertical',
+      route: '/operations',
+    },
+    {
+      label: 'EDA',
+      icon: 'bi-bar-chart-fill',
+      route: '/eda',
+    },
+  ];
+}
