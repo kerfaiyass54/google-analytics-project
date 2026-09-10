d306d3d399be543a383b0d507f5e29bbd56c4efe
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2026-09-10 16:01:34 +0100

    Added navbar for tha app and put it in the homepage

--- google-analytics-ui/src/app/shared/components/home-page/home-page.ts
+++ google-analytics-ui/src/app/shared/components/home-page/home-page.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { NavBar } from '../nav-bar/nav-bar';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [NavBar, RouterOutlet],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
